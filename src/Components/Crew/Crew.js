d58557b904ccc975c53526619a5457bc2e79bf1f
Mathias Roux <mathiasroux.pro@gmail.com>
2022-06-21 17:24:29 +0200

    responsive crew page

--- src/Components/Crew/Crew.js
+++ src/Components/Crew/Crew.js
@@ -13,22 +13,31 @@ export function Crew({step: number}) {
   return (
     <>
       <section className="crew">
-        <p className="title">
+
+        <div className="title__wrap">
+          <p className="title">
             <strong>02</strong>
             <span>meet you crew</span>
-        </p>
-        <img src={contents.images.png} alt={contents.name} />
-        <ul className="persons">
-          <li className={"dot " + (index===0?" active__person":"")} onClick={() => {setIndex(0)}}></li>
-          <li className={"dot " + (index===1?" active__person":"")} onClick={() => {setIndex(1)}}></li>
-          <li className={"dot " + (index===2?" active__person":"")} onClick={() => {setIndex(2)}}></li>
-          <li className={"dot " + (index===3?" active__person":"")} onClick={() => {setIndex(3)}}></li>
-        </ul>
-        <div className="infos">
-          <span className="info__role">{contents.role}</span>
-          <span className="info__names">{contents.name}</span>
-          <div className="description">
-            <p className="text">{contents.bio}</p>
+          </p> 
+        </div>
+        
+        <div className="crew__content">
+          <div className="crew__left">
+            <img src={contents.images.png} alt={contents.name} />
+          </div>
+
+          <div className="crew__right">
+            <ul className="persons">
+              <li className={"dot " + (index===0?" active__person":"")} onClick={() => {setIndex(0)}}></li>
+              <li className={"dot " + (index===1?" active__person":"")} onClick={() => {setIndex(1)}}></li>
+              <li className={"dot " + (index===2?" active__person":"")} onClick={() => {setIndex(2)}}></li>
+              <li className={"dot " + (index===3?" active__person":"")} onClick={() => {setIndex(3)}}></li>
+            </ul>
+            <div className="crew__infos">
+              <span className="info__role">{contents.role}</span>
+              <span className="info__names">{contents.name}</span>
+              <p className="crew__bio">{contents.bio}</p>
+            </div>
           </div>
         </div>
       </section>
